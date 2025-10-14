6f83d07daa7771595557784892efa093c8cffb3b
Robert Waffen <rw@betadots.de>
2025-10-14 15:16:00 +0200

    feat: use vendor free naming

--- container-entrypoint.sh
+++ container-entrypoint.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-if [ -d /docker-entrypoint.d/ ]; then
-    find /docker-entrypoint.d/ -type f -name "*.sh" \
+if [ -d /container-entrypoint.d/ ]; then
+    find /container-entrypoint.d/ -type f -name "*.sh" \
         -exec echo Running {} \; -exec sh {} \;
 fi
 
